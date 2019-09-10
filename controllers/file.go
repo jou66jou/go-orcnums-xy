3551d1feab9e9dcdea83d1916267c00437fe7a85
Jase Huang <jase@Jases-MacBook-Pro.local>
2019-09-11 02:28:11 +0800

    test0.0.1

--- controllers/file.go
+++ controllers/file.go
@@ -101,6 +101,7 @@ func FileUpload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// query與解析hORC的標準內容
 	dom, err := goquery.NewDocumentFromReader(strings.NewReader(out))
 
 	if err != nil {
@@ -120,12 +121,14 @@ func FileUpload(w http.ResponseWriter, r *http.Request) {
 		}
 	})
 
+	// 抓出圖片井字型1~9與0的位置
 	ocrsKeyboard, err := keepKeyNum(ocrs)
 	if err != nil {
 		errResp["error"] = err.Error()
 		render.JSON(http.StatusBadRequest, errResp)
 		return
 	}
+
 	render.JSON(http.StatusOK, map[string]interface{}{
 		"result":  ocrsKeyboard,
 		"version": version,
@@ -146,7 +149,7 @@ func getN(title string, num int) Num {
 func keepKeyNum(ocrsAll OCRs) (OCRs, error) {
 	var ocrsKeyboard = OCRs{}
 	var nbox [10][][]int
-	var rangeY = 10 // 寬容垂直座標的差值
+	var rangeY = 10 // 寬容水平座標的差值
 
 	// 箱子儲存法
 	for _, v := range ocrsAll.Nums {
@@ -159,17 +162,17 @@ func keepKeyNum(ocrsAll OCRs) (OCRs, error) {
 	// 	fmt.Println(k, "+", v)
 	// }
 
-	// 將垂直(Y1 + Y2) +- rangeY 的座標存到pointsY map, 以下方法建立在鍵盤同垂直上不會重複數字的情況
+	// 將水平(Y1 + Y2) +- rangeY 的座標存到pointsY map, 以下方法建立在鍵盤同同一水平上不會重複數字的情況
 	var pointsY = map[int][10][]int{} // { "pointsY" : [ yyy1:[x1,y1,x2,y2], ..., yyyN:[x1,y1,x2,y2] ]}
 
 	// 遍歷 1~10 box，數字0最後檢查
 	for i := 1; i < 10; i++ {
-		// 檢查畫面上數字每個出現位置的垂直座標
+		// 檢查畫面上數字每個出現位置的水平座標
 		for _, nP := range nbox[i] {
-			Y := nP[1] + nP[3] // 垂直座標(Y1 + Y2)
+			Y := nP[1] + nP[3] // 水平座標(Y1 + Y2)
 			noKey := true
 			for k, v := range pointsY {
-				// 檢查+-rangeY內的垂直座標，若有則加入同垂直座標的群組
+				// 檢查+-rangeY內的水平座標，若有則加入同水平座標的群組
 				if k+rangeY > Y && k-rangeY < Y {
 					v[i] = nP
 					pointsY[k] = v
@@ -177,7 +180,7 @@ func keepKeyNum(ocrsAll OCRs) (OCRs, error) {
 					break
 				}
 			}
-			// 若沒發現已有的垂直座標則新增 pointsY Map key by X
+			// 若沒發現已有的水平座標則新增 pointsY Map key by X
 			if noKey {
 				ypV := [10][]int{}
 				ypV[i] = nP
@@ -190,7 +193,7 @@ func keepKeyNum(ocrsAll OCRs) (OCRs, error) {
 		"456": [][]int{},
 		"789": [][]int{},
 	}
-	// 處理每個垂直相近的Num, 每當1,4,7為起始, 連續三個數字有紀錄則新增至combNum
+	// 處理每個水平相近的Num, 每當1,4,7為起始, 連續三個數字有紀錄則新增至combNum
 	for _, yNum := range pointsY {
 		combStr, startIndexs := check3Combo(yNum, 1, 4, 7)
 		for i, s := range combStr {
@@ -226,13 +229,13 @@ func keepKeyNum(ocrsAll OCRs) (OCRs, error) {
 			return ocrsKeyboard, errors.New("not found number : 0")
 		}
 	} else {
-		// TODO: 發現垂直座標相同的數組有多個或缺少，需再確認數組之間水平座標
+		// TODO: 發現水平座標相同的數組有多個或缺少，需再確認數組之間垂直座標
 		return ocrsKeyboard, errors.New("find multiple same or shortage of numbers by y point line")
 	}
 	return ocrsKeyboard, nil
 }
 
-// 確認該垂直有連續三個數字，回傳數組字串及起始位置slice
+// 確認該水平有連續三個數字，回傳數組字串及起始位置slice
 func check3Combo(yNum [10][]int, indexs ...int) ([]string, []int) {
 	var comboStr = map[int]string{
 		1: "123",
